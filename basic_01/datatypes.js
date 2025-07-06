8597fa424a46c870f105e10c735f4bf4b3cc4afd
MayurSaibane <157046166+MayurSaibane@users.noreply.github.com>
2025-07-06 13:33:17 +0000

    datatypes and notes

---
+++ basic_01/datatypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all js code as new versions
+let name = "mayur"
+let age=23
+let islogged="false"
+let state;
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+// Storing or printing text => "Hello" or 'Mayur'
+// Comparing with string values => if (name === "Mayur")
+// Returning string from a function => return "Done"
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log({ age :typeof age, name : typeof name, islogged : typeof islogged })
---
+++ basic_01/variables.js
@@ -0,0 +1,12 @@
+const accountId=5556
+let accountEmail="mayursaibane@gmail.com"
+var accountpassword="..23@#2"
+accountCity="Pune"
+let accountState="Maharashtra"
+
+//let accountId=32425
+console.table([accountId,accountEmail,accountpassword,accountCity,accountState])
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
