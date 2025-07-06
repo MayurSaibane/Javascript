8d828f8cc406a14d4c654121c955e3160718d0a6
MayurSaibane <157046166+MayurSaibane@users.noreply.github.com>
2025-07-06 17:05:29 +0000

    conersions

---
+++ basic_01/conversions.js
@@ -0,0 +1,32 @@
+let score ="33asd"
+
+console.log(typeof (score))
+console.log(typeof score)
+
+let valueOfNumber =Number(score) // for conversion use cpital case for conversion type
+console.log(typeof valueOfNumber)
+console.log(valueOfNumber)
+/*
+NOTES
+"33"=33
+"33abc"=NaN
+null=0
+undefined=NaN
+boolean=> True=1 , false=0
+
+*/
+let islogged=""                        // if its leave empty  with no value such as let islogged="" asnwer is false  
+let isloggedBoolean=Boolean(islogged)
+console.log(typeof isloggedBoolean);
+console.log(isloggedBoolean);
+// 1 => true; 0 => false
+// "" => false
+// "Mayur" => true
+ 
+let N1=24
+let convertString= String(N1)
+
+console.log(typeof convertString);
+console.log(convertString);
+
+
