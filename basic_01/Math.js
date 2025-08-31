e7694ff1f81f984b2a2e3630fc0a76a38f13fbf2
Mayur Baburao Saibane <157046166+MayurSaibane@users.noreply.github.com>
2025-08-31 15:08:52 +0000

    Math.js

---
+++ basic_01/Math.js
@@ -0,0 +1,15 @@
+/*
+console.log(Math);
+console.log(Math.abs(-4));// convert -ve to +ve but no vice versa  
+console.log(Math.round(4.6)); // ROundoff according to general rule 
+console.log(Math.round(4.4));
+console.log(Math.floor(4.6));// reduce to original left value even if 4.9 o/p is 4 
+console.log(Math.ceil(4.2));//increse to next vaule in these case 5
+console.log(Math.max(4,6,8,3,1));
+console.log(Math.min(4,9,2,3,8));
+*/
+console.log(Math.random());//Random value comes betwen 0 and 1
+console.log(Math.floor(Math.random()*10) +1); 
+const Min=10;
+const Max=20;
+console.log(Math.floor(Math.random()*(Max-Min+1))+Min);
